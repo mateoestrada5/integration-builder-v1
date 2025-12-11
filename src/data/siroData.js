99a9a18436930a47c1ba3a2f82704a1442201111
Mateo Estrada <mateoestrada1403@gmail.com>
2025-12-11 03:44:55 -0300

    Update description in SIRO_DATA for version

--- src/data/siroData.js
+++ src/data/siroData.js
@@ -11,7 +11,7 @@ export const SIRO_DATA = {
   id: 'root',
   title: 'Nodo Raíz',
   type: 'root',
-  description: 'Este es el nodo principal del árbol. Aquí iría la descripción general del servicio.',
+  description: 'Este es el nodo principal del árbol. Aquí iría la descripción general del servicio V1.',
   children: [
     {
       id: 'node_1',
